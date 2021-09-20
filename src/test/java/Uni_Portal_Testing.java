0ad01740934e0886d5589102e805d7721e7a0265
Fardin Amin Arpon <fardinaminarpon@ieee.org>
2021-09-21 03:40:02 +0600

    Portal Automation

--- src/test/java/Uni_Portal_Testing.java
+++ src/test/java/Uni_Portal_Testing.java
@@ -1,4 +1,5 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -19,16 +20,6 @@ public class Uni_Portal_Testing {
     WebDriver driver;
     WebDriverWait wait;
 
-//    @Before
-//    public void setup_Firefox_GeckoDriver() {
-//        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
-//        FirefoxOptions ops = new FirefoxOptions();
-//        ops.addArguments("--headed");
-//        driver = new FirefoxDriver(ops);
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-//    }
-
     @Before
     public void setup_ChromeDriver(){
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
@@ -43,98 +34,50 @@ public class Uni_Portal_Testing {
     public void PortalsTesting() throws InterruptedException {
         driver.get("http://173.254.242.107:81/index.php?logout=1");
         wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(2) td:nth-child(3) > input.class1"))).sendKeys("191002065");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(3) td:nth-child(3) > input.class1"))).sendKeys("bAAAl");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(4) td:nth-child(1) center:nth-child(3) > input.class1:nth-child(1)"))).sendKeys(Keys.ENTER);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(2) td:nth-child(3) > input.class1"))).sendKeys("201002243");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(3) td:nth-child(3) > input.class1"))).sendKeys("Arpon243");
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(4) td:nth-child(1) center:nth-child(3) > input.class1:nth-child(1)"))).sendKeys(Keys.ENTER);
+        Boolean Home = driver.findElement(By.xpath("//b[contains(text(),'Semester:')]")).isDisplayed();
+        Assert.assertEquals(Home, true);
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Preregistration')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
+        boolean Preregistration = driver.findElement(By.xpath("//p[contains(text(),'Preregistration')]")).isDisplayed();
+        Assert.assertEquals(Preregistration, true);
+        sleep(2000);
+
+
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Schedule')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
+        Boolean Schedule = driver.findElement(By.xpath("//p[contains(text(),'Class Schedule')]")).isDisplayed();
+        Assert.assertEquals(Schedule, true);
+        sleep(2000);
+
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Billing')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Result')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
+        Boolean Billing = driver.findElement(By.xpath("//p[contains(text(),'Billing History')]")).isDisplayed();
+        Assert.assertEquals(Billing, true);
+        sleep(2000);
+
+        //wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Result')]"))).sendKeys(Keys.ENTER);
+        //sleep(3000);
+
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Profile')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
+        Boolean Profile = driver.findElement(By.xpath("//p[contains(text(),'My Profile')]")).isDisplayed();
+        Assert.assertEquals(Profile, true);
+        sleep(2000);
+
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Teacher Evaluation')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
+        Boolean Evaluation = driver.findElement(By.xpath("//p[contains(text(),'Teacher Evaluation Submission')]")).isDisplayed();
+        Assert.assertEquals(Evaluation, true);
+        sleep(2000);
+
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Logout')]"))).sendKeys(Keys.ENTER);
+        Boolean PortalPage = driver.findElement(By.xpath("//tbody//tr//td//h3")).isDisplayed();
+        Assert.assertEquals(PortalPage, true);
+
     }
-
-
-    @Test
-    public void PortalTesting_Multipal() throws InterruptedException {
-        driver.get("http://173.254.242.107:81/index.php?logout=1");
-        wait = new WebDriverWait(driver, 10);
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(2) td:nth-child(3) > input.class1"))).sendKeys("191002065");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(3) td:nth-child(3) > input.class1"))).sendKeys("bAAAl");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(4) td:nth-child(1) center:nth-child(3) > input.class1:nth-child(1)"))).sendKeys(Keys.ENTER);
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Preregistration')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Schedule')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Billing')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Result')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Profile')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Teacher Evaluation')]"))).sendKeys(Keys.ENTER);
-        sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Logout')]"))).sendKeys(Keys.ENTER);
-
-
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(2) td:nth-child(3) > input.class1"))).sendKeys("191002065");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(3) td:nth-child(3) > input.class1"))).sendKeys("bAAAl");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(4) td:nth-child(1) center:nth-child(3) > input.class1:nth-child(1)"))).sendKeys(Keys.ENTER);
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Preregistration')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Schedule')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Billing')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Result')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Profile')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Teacher Evaluation')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Logout')]"))).sendKeys(Keys.ENTER);
-
-
-
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(2) td:nth-child(3) > input.class1"))).sendKeys("201002145");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(3) td:nth-child(3) > input.class1"))).sendKeys("ssuumm");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("table.login tbody:nth-child(1) tr:nth-child(4) td:nth-child(1) center:nth-child(3) > input.class1:nth-child(1)"))).sendKeys(Keys.ENTER);
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Preregistration')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Schedule')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Billing')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Result')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Profile')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Teacher Evaluation')]"))).sendKeys(Keys.ENTER);
-            sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Logout')]"))).sendKeys(Keys.ENTER);
-    }
-
 
     @After
-    public void finishTest() {
+    public void finishTest(){
         driver.close();
     }
 }
-
-
-
